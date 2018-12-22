26a28e2886fe484044a229f4b3943a9fa8e2570a
Shay Ben-Simon <shay.bs@spot.im>
2018-12-22 15:50:06 +0200

    isTwoEdgeConnected

--- src/algorithms/graph.js
+++ src/algorithms/graph.js
@@ -1,33 +1,53 @@
 import jsgraphs from 'js-graph-algorithms';
 import math from 'mathjs';
+import _ from 'lodash';
 
 export class Graph {
-  constructor() {
-    this.graph = new jsgraphs.Graph(3);
+  constructor(noOfNodes = 3) {
+    this.graph = new jsgraphs.Graph(noOfNodes);
   }
 
-  main = (debug, noOfNodes = 3) => {
-    this.init(noOfNodes);
+  main = (debug) => {
+    this.init();
     const g = this.graph;
     if (debug) {
       debugger;
     }
   };
 
-  init = (noOfNodes) => {
+  init = () => {
     const g = this.graph;
+    g.addEdge(0, 1);
+    g.addEdge(0, 2);
+    g.addEdge(1, 2);
+    g.node(0).label = 'n0';
+    g.node(1).label = 'n1';
+    g.node(2).label = 'n2';
+  };
 
-    // init edges
-    for (let i = 0; i < noOfNodes; i++) {
-      for (let j = 0; j < noOfNodes; j++) {
-        if (i !== j && !g.edge(i, j)) {
-          g.addEdge(i, j);
+  isTwoEdgeConnected = () => {
+    const g = this.graph;
+    const cc = new jsgraphs.ConnectedComponents(g);
+    if (cc.componentCount() > 1) return false;
+    for (let i = 0; i < g.V; i++) {
+      for (let j = 0; j < g.V; j++) {
+        if (g.edge(i, j)) {
+          let newG = this.cloneGraph(g);
+          this.removeEdge(newG, i, j);
+          let newCC = new jsgraphs.ConnectedComponents(newG);
+          if (newCC.componentCount() > 1) return false;
         }
       }
     }
-    // label nodes
-    for (let i = 0; i < noOfNodes; i++) {
-      g.node(i).label = `n${i}`;
-    }
+    return true;
+  };
+
+  cloneGraph = (g) => _.cloneDeep(g);
+
+  removeEdge = (g, l, r) => {
+    const rIndex = g.adjList[l].indexOf(r);
+    const lIndex = g.adjList[r].indexOf(l);
+    g.adjList[l].splice(rIndex, 1);
+    g.adjList[r].splice(lIndex, 1);
   };
 }
